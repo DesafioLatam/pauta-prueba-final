e1f77d9f618425a542eee1fa3f501be22dd3a521
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-03-27 17:45:26 -0300

    change the bidder ids fixture to the user two (user one is not a bidder)

--- test/models/bid_test.rb
+++ test/models/bid_test.rb
@@ -6,20 +6,20 @@ class BidTest < ActiveSupport::TestCase
   end
 
   test "cannot bid after product expiration " do
-    @bid = Bid.new(product: @product, user:users(:one), created_at: Time.now + 3.hours)
+    @bid = Bid.new(product: @product, user:users(:two), created_at: Time.now + 3.hours)
     assert !@bid.valid?, "cannot bid after expiration"
   end
 
   test "same user cannot bid two consecutive times" do
-    @bid = Bid.create(user: users(:one), product: products(:one))
-    @bid2 = Bid.create(user: users(:one), product: products(:one))
+    @bid = Bid.create(user: users(:two), product: products(:one))
+    @bid2 = Bid.create(user: users(:two), product: products(:one))
     assert !@bid2.valid?, "same user cannot bid two consecutive times"
   end
 
   test "same user can bid if another user bid after him" do
-    @bid = Bid.create(user: users(:one), product: products(:one))
-    @bid2 = Bid.create(user: users(:one), product: products(:one))
-    @bid3 = Bid.create(user: users(:one), product: products(:one))
+    @bid = Bid.create(user: users(:two), product: products(:one))
+    @bid2 = Bid.create(user: users(:two), product: products(:one))
+    @bid3 = Bid.create(user: users(:two), product: products(:one))
     assert @bid3.valid?, "user can bid if another user bid after him"
   end
 
