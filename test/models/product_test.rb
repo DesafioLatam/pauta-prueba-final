cf0bfce5c3d6533e20e83e554df9c03510eef2a0
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-03-27 16:52:59 -0300

    validation for product name

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product = products(:one)
+  end
+
+  test "should have name" do
+    @product.name = nil
+    assert !@product.valid?, "product name cannot be nil"
+  end
 end
