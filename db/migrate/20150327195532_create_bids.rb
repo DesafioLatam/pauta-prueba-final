154aa3abd3707b05c77173b57dbb095df6eeb0e8
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-03-27 16:56:24 -0300

    bids model with relations to user and product

---
+++ db/migrate/20150327195532_create_bids.rb
@@ -0,0 +1,12 @@
+class CreateBids < ActiveRecord::Migration
+  def change
+    create_table :bids do |t|
+      t.references :user, index: true
+      t.references :product, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :bids, :users
+    add_foreign_key :bids, :products
+  end
+end
